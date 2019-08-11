9fdb40e713c3b2302af0afe4fa4aca2ab7ddc823
Bry Cox <carl.cc@gmail.com>
2019-08-11 22:01:08 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 
   validates :title, presence: true
   validates :content, length: { minimum: 25 }
-  validates :category, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 
 end
